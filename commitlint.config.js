0d1ba62bed4858224458e96832ffc175018c8860
Anonymous <anonymous@example.com>
2025-12-20 08:52:50 +0800

    ci: 规范化代码提交

---
+++ commitlint.config.js
@@ -0,0 +1,29 @@
+module.exports = {
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    'type-enum': [
+      2,
+      'always',
+      [
+        'feat',     // 新功能
+        'fix',      // 修复bug
+        'docs',     // 文档变更
+        'style',    // 代码格式（不影响代码运行的变动）
+        'refactor', // 重构（既不是新增功能，也不是修复bug的代码变动）
+        'perf',     // 性能优化
+        'test',     // 增加测试
+        'chore',    // 构建过程或辅助工具的变动
+        'revert',   // 回滚
+        'build',    // 构建系统或外部依赖的更改
+        'ci',       // CI配置文件和脚本的更改
+      ],
+    ],
+    'type-case': [2, 'always', 'lower-case'],
+    'type-empty': [2, 'never'],
+    'scope-case': [2, 'always', 'lower-case'],
+    'subject-empty': [2, 'never'],
+    'subject-full-stop': [2, 'never', '.'],
+    'header-max-length': [2, 'always', 100],
+  },
+};
+
